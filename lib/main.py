ed33f7af805f73dab8e17d3f6d50792a557165d3
Leonid Astrin <astrinleonid@gmail.com>
2023-04-24 13:47:15 +0300

    Telegram 1

--- lib/main.py
+++ lib/main.py
@@ -2,15 +2,22 @@ from flask import Flask
 from flask import request
 from flask import send_file
 from flask import render_template
-from flask import Response
-
 import pickle
-
 import openai
-with open('openaitoken.txt') as token_file:
-    openai.api_key = token_file.read()
+import json
+import requests
+from flask import Response
+from telegram import Update, Bot
+from telegram.ext import Updater, CommandHandler, MessageHandler, filters
 
-COQUI_STUDIO_TOKEN='aexT3jUUjnq0rvbzcMVV3p74SOs398EryywqBqrNK2dhMxyD5p9ke8cR4uzcrxQn'
+
+with open('config.json') as token_file:
+    tokens = json.load(token_file)
+
+openai.api_key = tokens['openai']
+telegram_token  = tokens['telegram']
+COQUI_STUDIO_TOKEN = tokens['coqui']
+
 speakers = {'LEONID' : 'kingsson.wav', 'NOF' : 'Nof.wav', 'SASHA' : 'sasha.wav'}
 
 with open('tts_model.pkl','rb' ) as file:
@@ -53,7 +60,27 @@ def get_response():
     return send_file(output_file, mimetype='audio/wav', as_attachment=True)
 
 
-app.run(host='0.0.0.0', port=5001)
+updater = Updater(token=telegram_token, use_context=True)
+dispatcher = updater.dispatcher
+
+def handle_text(update, context):
+    # You can replace this part with your own logic to generate a response
+    response_text = f'You said: {update.message.text}'
+    context.bot.send_message(chat_id=update.message.chat_id, text=response_text)
+
+text_handler = MessageHandler(filters.text, handle_text)
+dispatcher.add_handler(text_handler)
+
+@app.route(f'/{telegram_token}', methods=['POST'])
+def handle_telegram_update():
+    update = Update.de_json(request.get_json(force=True), updater.bot)
+    dispatcher.process_update(update)
+    return 'ok'
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5001)
+
+
 
 
 
