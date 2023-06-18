e541d1dfa54d43ed7830f31c7c786792dcece353
Leonid Astrin <astrinleonid@gmail.com>
2023-06-18 11:53:44 +0300

    Pickle

--- lib/telegram_bot_stable.py
+++ lib/telegram_bot_stable.py
@@ -1,9 +1,16 @@
 import pickle
+import os
+import sys
+
+current_folder = os.getcwd()
+# Add the ffmpeg path to the system PATH
+sys.path.append(current_folder + '\\ffmpeg')
+os.environ['PATH'] += ';' + current_folder + '\\ffmpeg'
 import openai
 import json
 import requests
 import telebot
-import os
+
 import platform
 import pathlib
 import time
@@ -13,15 +20,21 @@ from TTS.api import TTS
 
 import whisper
 from pydub import AudioSegment
+# AudioSegment.converter = ffmpeg_path
+# AudioSegment.ffmpeg = ffmpeg_path
+# AudioSegment.ffprobe = ffprobe_path
 from voice_generator import Voice, Synthesizer
 
 current_os = platform.system()
+
 print("Current OS: ", current_os)
 if current_os == "Windows":
     temp = pathlib.PosixPath
     pathlib.PosixPath = pathlib.WindowsPath
 
-voice_transcription_model = whisper.load_model("small")
+# voice_transcription_model = whisper.load_model("small")
+with open('whisper_model.pkl', 'rb') as file:
+    voice_transcription_model = pickle.load(file)
 
 with open('config.json') as token_file:
     tokens = json.load(token_file)
@@ -67,28 +80,28 @@ now = datetime.now()
 dt_string = now.strftime("%d/%m/%Y %H:%M:%S")
 error_message = f"Bot started at {dt_string}"
 
-def bot_polling(token=telegram_token):
 
-     global error_message
-     bot = None
-     print("Starting bot polling now")
-     while True:
-         try:
-             print("New bot instance started")
-             bot = telebot.TeleBot(token) #Generate new bot instance
-             botactions(bot) #If bot is used as a global variable, remove bot as an input param
-             bot.polling(none_stop=True, interval=BOT_INTERVAL, timeout=BOT_TIMEOUT)
-         except Exception as ex: #Error in polling
-             print("Bot polling failed, restarting in {}sec. Error:\n{}".format(BOT_TIMEOUT, ex))
-             now = datetime.now()
-             dt_string = now.strftime("%d/%m/%Y %H:%M:%S")
-             error_message += f"\n\nCrash at {dt_string} Error message: {ex}"
-             bot.stop_polling()
-             time.sleep(BOT_TIMEOUT)
-         else: #Clean exit
-             bot.stop_polling()
-             print("Bot polling loop finished")
-             break #End loop
+def bot_polling(token=telegram_token):
+    global error_message
+    bot = None
+    print("Starting bot polling now")
+    while True:
+        try:
+            print("New bot instance started")
+            bot = telebot.TeleBot(token)  # Generate new bot instance
+            botactions(bot)  # If bot is used as a global variable, remove bot as an input param
+            bot.polling(none_stop=True, interval=BOT_INTERVAL, timeout=BOT_TIMEOUT)
+        except Exception as ex:  # Error in polling
+            print("Bot polling failed, restarting in {}sec. Error:\n{}".format(BOT_TIMEOUT, ex))
+            now = datetime.now()
+            dt_string = now.strftime("%d/%m/%Y %H:%M:%S")
+            error_message += f"\n\nCrash at {dt_string} Error message: {ex}"
+            bot.stop_polling()
+            time.sleep(BOT_TIMEOUT)
+        else:  # Clean exit
+            bot.stop_polling()
+            print("Bot polling loop finished")
+            break  # End loop
 
 
 file_path = 'response'
@@ -106,6 +119,7 @@ synth.add_voice(Voice('Aria', 'azure', "en-US-AriaNeural"))
 
 default_mode = 'chatgpt'
 user_modes = {}
+
 
 def return_voice_response(prompt, mode):
     x = []
@@ -155,24 +169,23 @@ def return_voice_response(prompt, mode):
     synth.generate(x)
     return synth.get_file_path()
 
-def botactions(bot):
 
+def botactions(bot):
     @bot.message_handler(commands=['new_chat'])
     def new_chat(message):
         chatGPT.new_chat()
         bot.send_message(message.chat.id, "Start a new conversation", parse_mode='html')
 
-
     @bot.message_handler(commands=['start'])
     def start(message):
         greeting = """
             <b>Welcome to the chatGPT chatbot</b>
-    
+
             Chatbot has three modes. 
             <b>ChatGPT mode:</b> any text or voice input is treated as a prompt for chatGPT. The response received from chatGPT API is vocalized and returned as the sound file
             <b>Parrot mode:</b> the input itself is converted to speech and returned as a sound file
             <b>Speech to text mode:</b> the voice input is converted to text
-    
+
             To change mode: /mode
             To change speaker: /speaker
             To change chatGPT model: /model
@@ -180,7 +193,6 @@ def botactions(bot):
             To start a new conversation with chatGPT: /new_chat
       """
         bot.send_message(message.chat.id, greeting, parse_mode='html')
-
 
     @bot.message_handler(commands=['speaker'])
     def get_speaker(message):
@@ -190,9 +202,9 @@ def botactions(bot):
             all_speakers += f"{i + 1} {voice.name} {voice.model}\n"
         bot.send_message(message.chat.id, f'Current speaker: {synth.get_voice_name()}', parse_mode='html')
         bot.send_message(message.chat.id, f'Available speakers: {all_speakers}', parse_mode='html')
-        sent = bot.reply_to(message, f'To change speaker, reply with speaker number, else reply with 0', parse_mode='html')
+        sent = bot.reply_to(message, f'To change speaker, reply with speaker number, else reply with 0',
+                            parse_mode='html')
         bot.register_next_step_handler(sent, change_speaker)
-
 
     @bot.message_handler(commands=['new_speaker'])
     def new_speaker(message):
@@ -210,7 +222,6 @@ def botactions(bot):
                             parse_mode='html')
         bot.register_next_step_handler(sent, new_speaker)
 
-
     # markup = types.KeyboardButtonPollType
 
     @bot.message_handler(commands=['mode'])
@@ -226,7 +237,6 @@ def botactions(bot):
                             parse_mode='html')
         bot.register_next_step_handler(sent, change_mode)
         print("Mode changed")
-
 
     @bot.message_handler(commands=['model'])
     def get_model(message):
@@ -254,7 +264,6 @@ def botactions(bot):
 
         bot.send_message(message.chat.id, error_message)
 
-
     @bot.message_handler(content_types=['text'])
     def get_user_text(message):
         print("Incoming text")
@@ -267,29 +276,27 @@ def botactions(bot):
         audio = open(output_file, 'rb')
         bot.send_audio(message.chat.id, audio)
 
-
     @bot.message_handler(content_types=['voice'])
     def get_user_voice(message):
-       print("Incoming voice")
-       file_id = message.voice.file_id
-       file_name = f'voice_{file_id}.ogg'
-       # Download the voice message file
-       prompt_file = download_voice_file(file_id, file_name)
-       print('File saved')
-       result = voice_transcription_model.transcribe(prompt_file)
-       prompt = " ".join([segment['text'] for segment in result['segments']])
-       print(prompt)
+        print("Incoming voice")
+        file_id = message.voice.file_id
+        file_name = f'voice_{file_id}.ogg'
+        # Download the voice message file
+        download_voice_file(file_id, file_name)
+        print('File saved')
+        result = voice_transcription_model.transcribe('voice_prompt.wav')
+        prompt = " ".join([segment['text'] for segment in result['segments']])
+        print(prompt)
 
-       mode = get_user_mode(message.chat.id)
+        mode = get_user_mode(message.chat.id)
 
-       if mode == "speech_to_text":
-           bot.send_message(message.chat.id, prompt, parse_mode='html')
-       else:
-           output_file = return_voice_response(prompt, mode)
-           # bot.send_message(message.chat.id, 'Audio follows', parse_mode = 'html')
-           audio = open(output_file, 'rb')
-           bot.send_audio(message.chat.id, audio)
-
+        if mode == "speech_to_text":
+            bot.send_message(message.chat.id, prompt, parse_mode='html')
+        else:
+            output_file = return_voice_response(prompt, mode)
+            # bot.send_message(message.chat.id, 'Audio follows', parse_mode = 'html')
+            audio = open(output_file, 'rb')
+            bot.send_audio(message.chat.id, audio)
 
     def change_mode(message):
         global user_modes
@@ -303,7 +310,6 @@ def botactions(bot):
             pass
         bot.send_message(message.chat.id, f'Current mode: {user_modes[message.chat.id]}', parse_mode='html')
 
-
     def change_speaker(message):
         try:
             num = int(message.text)
@@ -314,7 +320,6 @@ def botactions(bot):
         except:
             pass
         bot.send_message(message.chat.id, f'Current speaker: {synth.get_voice_name()}', parse_mode='html')
-
 
     def new_speaker(message):
         print(message.content_type)
@@ -330,7 +335,6 @@ def botactions(bot):
             bot.register_next_step_handler(sent, add_speaker_to_the_list)
         else:
             bot.send_message(message.chat.id, f'Current speaker: {synth.get_voice_name()}', parse_mode='html')
-
 
     def add_speaker_to_the_list(message):
         speaker_name = message.text
@@ -352,7 +356,6 @@ def botactions(bot):
                                     parse_mode='html')
             bot.register_next_step_handler(sent, add_speaker_to_the_list)
 
-
     def change_model(message):
         global model
         try:
@@ -364,18 +367,16 @@ def botactions(bot):
             pass
         bot.send_message(message.chat.id, f'Current model: {model}', parse_mode='html')
 
-
-
     def download_voice_file(file_id, file_name, output_file='voice_prompt.wav'):
         file_path = bot.get_file(file_id).file_path
         file_url = f'https://api.telegram.org/file/bot{telegram_token}/{file_path}'
-
         response = requests.get(file_url)
-
+        # print(response)
         with open(file_name, 'wb') as f:
             f.write(response.content)
-        return file_name
-
+        # audio = AudioSegment.from_file(file_name, ffprobe=ffprobe_path)
+        audio = AudioSegment.from_file(file_name)
+        audio.export(output_file, format='wav')
 
     def get_user_mode(id):
         global user_modes
@@ -383,17 +384,14 @@ def botactions(bot):
             user_modes[id] = default_mode
         return user_modes[id]
 
+
 if __name__ == '__main__':
-    # polling_thread = threading.Thread(target=bot_polling)
-    # polling_thread.daemon = True
-    # polling_thread.start()
-    # app.run(host='0.0.0.0', port=port)
+
     bot_polling(telegram_token)
-    # while True:
-    #     try:
-    #         time.sleep(120)
-    #     except KeyboardInterrupt:
-    #         break
+
+
+
+
 
 
 
